9da5bc8779b0e7f203900f56256c55ce063621cf
johel <johel.carvalho@gmail.com>
2016-05-18 09:07:10 -0300

    simple demonstration of a working modal window

--- src/components/posts-list.js
+++ src/components/posts-list.js
@@ -3,12 +3,48 @@ import {connect} from 'react-redux';
 import {fetchPosts} from '../actions';
 import {Link} from 'react-router';
 import Loader from 'react-loader';
+import Modal from 'react-modal';
+
+
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
 
 class PostsList extends Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {modalIsOpen:false};
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+  }
 
 	componentDidMount(){
 		this.props.dispatch(fetchPosts());
 	}
+
+  openModal(){
+    this.setState({modalIsOpen: true});
+  }
+
+  closeModal(){
+    this.setState({modalIsOpen: false});
+  }
+
+  afterOpenModal(){
+       // references are now sync'd and can be accessed.
+    this.refs.subtitle.style.color = '#f00';
+  }
 
 
   renderPosts() {
@@ -27,21 +63,39 @@ class PostsList extends Component{
   render() {
     return (
       <Loader loaded={!this.props.list.isLoading}>
-      <div className="container">
-        <div className="row text-right">
-          <Link to="/posts/new" className="btn btn-primary">
-            Adicione um Post
-          </Link>
+        <div className="container">
+          <div className="row text-right">
+            <Link to="/posts/new" className="btn btn-primary">
+              Adicione um Post
+            </Link>
+          </div>
+          <h3>Posts</h3>
+          <ul className="list-group">
+  			    <li className="list-group-item row">
+  			        <strong className="col-md-6">Categorias</strong>
+  			        <strong className="col-md-6">Titulo</strong>
+  			    </li>
+            {this.renderPosts()}
+          </ul>
         </div>
-        <h3>Posts</h3>
-        <ul className="list-group">
-			    <li className="list-group-item row">
-			        <strong className="col-md-6">Categorias</strong>
-			        <strong className="col-md-6">Titulo</strong>
-			    </li>
-          {this.renderPosts()}
-        </ul>
-      </div>
+                 <button onClick={this.openModal}>Open Modal</button>
+          <Modal
+            isOpen={this.state.modalIsOpen}
+            onAfterOpen={this.afterOpenModal}
+            onRequestClose={this.closeModal}
+            style={customStyles}>
+
+            <h2 ref="subtitle">Hello</h2>
+            <button onClick={this.closeModal}>close</button>
+            <div>I am a modal</div>
+            <form>
+              <input />
+              <button>tab navigation</button>
+              <button>stays</button>
+              <button>inside</button>
+              <button>the modal</button>
+            </form>
+          </Modal>
       </Loader>
     );
   }
